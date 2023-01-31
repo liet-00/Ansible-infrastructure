243ba6158d41fc217159145c1afe6d4c47ee29e6
nhan <liet@pop-os.localdomain>
2023-01-31 15:53:32 +0100

    removing files

--- fingerprint.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-ssh-keygen -f "/home/liet/.ssh/known_hosts" -R "192.168.60.2"
-ssh-keygen -f "/home/liet/.ssh/known_hosts" -R "192.168.60.3"
-ssh-keygen -f "/home/liet/.ssh/known_hosts" -R "192.168.60.4"
-
-vagrant destroy --force
-vagrant up
-
